312fbd6bcbf1f545c450de1f4242016744a6f082
Raja Asyraf <rmasyraf@gmail.com>
2024-06-25 21:00:40 +0800

    feat: update pagination and display all products

--- src/components/ProductTable.vue
+++ src/components/ProductTable.vue
@@ -1,4 +1,29 @@
 <script setup>
+import { ref, onMounted, watch } from 'vue'
+import axios from 'axios'
+import { Bootstrap5Pagination } from 'laravel-vue-pagination'
+
+const products = ref([])
+const pagination = ref([])
+const perPage = ref(10)
+
+watch(perPage, () => {
+  getProducts()
+})
+
+const getProducts = async (page = 1) => {
+  try {
+    let response = await axios.get(
+      `http://localhost/api/product?page=${page}&perPage=${perPage.value}`
+    )
+    pagination.value = response.data
+    products.value = pagination.value.data
+  } catch (error) {
+    console.error('Error fetching data:', error)
+  }
+}
+
+onMounted(getProducts)
 </script>
 
 <template>
@@ -13,11 +38,11 @@
             Show
             <div class="mx-2 d-inline-block">
               <input
-                type="text"
+                type="number"
                 class="form-control form-control-sm"
-                value="8"
+                v-model="perPage"
                 size="3"
-                aria-label="Invoices count"
+                aria-label="Product per page"
               />
             </div>
             entries
@@ -34,6 +59,7 @@
         <table class="table card-table table-vcenter text-nowrap datatable">
           <thead>
             <tr>
+              <th>No</th>
               <th class="w-1">
                 Product ID
                 <!-- Download SVG icon from http://tabler-icons.io/i/chevron-up -->
@@ -61,69 +87,27 @@
             </tr>
           </thead>
           <tbody>
-            <tr>
-              <td><span class="text-secondary">001401</span></td>
-              <td>Smartphone</td>
-              <td>Apple</td>
-              <td>iPhone 12</td>
-              <td>2GB/16GB</td>
-              <td>10</td>
+            <tr v-for="(product, index) in products" :key="index">
+              <td>{{ product.id }}</td>
+              <td>
+                <span class="text-secondary">{{ product.product_id }}</span>
+              </td>
+              <td>{{ product.type }}</td>
+              <td>{{ product.product_model.brand.name }}</td>
+              <td>{{ product.product_model.name }}</td>
+              <td>{{ product.capacity }}</td>
+              <td>{{ product.quantity }}</td>
             </tr>
           </tbody>
         </table>
       </div>
       <div class="card-footer d-flex align-items-center">
         <p class="m-0 text-secondary">
-          Showing <span>1</span> to <span>8</span> of <span>16</span> entries
+          Showing <span>{{ pagination.from }}</span> to <span>{{ pagination.to }}</span> of
+          <span>{{ pagination.total }}</span> entries
         </p>
         <ul class="pagination m-0 ms-auto">
-          <li class="page-item disabled">
-            <a class="page-link" href="#" tabindex="-1" aria-disabled="true">
-              <!-- Download SVG icon from http://tabler-icons.io/i/chevron-left -->
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                class="icon"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                stroke-width="2"
-                stroke="currentColor"
-                fill="none"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-              >
-                <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-                <path d="M15 6l-6 6l6 6"></path>
-              </svg>
-              prev
-            </a>
-          </li>
-          <li class="page-item"><a class="page-link" href="#">1</a></li>
-          <li class="page-item active"><a class="page-link" href="#">2</a></li>
-          <li class="page-item"><a class="page-link" href="#">3</a></li>
-          <li class="page-item"><a class="page-link" href="#">4</a></li>
-          <li class="page-item"><a class="page-link" href="#">5</a></li>
-          <li class="page-item">
-            <a class="page-link" href="#">
-              next
-              <!-- Download SVG icon from http://tabler-icons.io/i/chevron-right -->
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                class="icon"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                stroke-width="2"
-                stroke="currentColor"
-                fill="none"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-              >
-                <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-                <path d="M9 6l6 6l-6 6"></path>
-              </svg>
-            </a>
-          </li>
+          <Bootstrap5Pagination :data="pagination" @pagination-change-page="getProducts" />
         </ul>
       </div>
     </div>
