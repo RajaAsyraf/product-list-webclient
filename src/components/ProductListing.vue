a53a3ffbbf32b5cbe3751159161d8da99048fb29
Raja Asyraf <rmasyraf@gmail.com>
2024-06-26 09:51:11 +0800

    chore: display response and fix styling

--- src/components/ProductListing.vue
+++ src/components/ProductListing.vue
@@ -5,6 +5,8 @@ import { ref } from 'vue'
 
 const uploadedFile = ref(null)
 const productTable = ref(null)
+const showResponse = ref(false)
+const uploadResponse = ref('')
 
 const handleUploadFile = async () => {
   if (uploadedFile.value.files[0] === undefined) {
@@ -18,15 +20,17 @@ const handleUploadFile = async () => {
     url: 'http://localhost/api/product-sync',
     data: formData,
     config: { headers: { 'Content-Type': 'multipart/form-data' } }
-  }).then(() => {
+  }).then((response) => {
+    uploadResponse.value = response.data.message
+    showResponse.value = true
     productTable.value.getProducts()
   })
 }
 </script>
 
 <template>
-  <div class="mb-3 row">
-    <form action="" class="input-group" @submit.prevent="false">
+  <div class="mb-3 mx-1 row">
+    <form action="" class="form-control input-group" @submit.prevent="false">
       <div class="col-md-6">
         <input
           type="file"
@@ -38,10 +42,41 @@ const handleUploadFile = async () => {
           required
         />
       </div>
-      <div class="col-md-4">
+      <div class="col-md-2">
         <button class="input-group-text mx-2 btn btn-primary w-20" @click="handleUploadFile">
           Upload
         </button>
+      </div>
+      <div class="col-md-4" v-if="showResponse">
+        <div class="alert alert-success alert-dismissible mb-0" role="alert">
+          <div class="d-flex">
+            <div>
+              <!-- Download SVG icon from http://tabler-icons.io/i/check -->
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                class="icon alert-icon"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                stroke-width="2"
+                stroke="currentColor"
+                fill="none"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              >
+                <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                <path d="M5 12l5 5l10 -10"></path>
+              </svg>
+            </div>
+            <div>{{ uploadResponse }}</div>
+          </div>
+          <a
+            class="btn-close"
+            data-bs-dismiss="alert"
+            aria-label="close"
+            @click="showResponse = false"
+          ></a>
+        </div>
       </div>
     </form>
   </div>
--- src/components/ProductTable.vue
+++ src/components/ProductTable.vue
@@ -43,75 +43,77 @@ defineExpose({
 </script>
 
 <template>
-  <div class="col-12">
-    <div class="card">
-      <div class="card-header">
-        <h3 class="card-title">Products</h3>
-      </div>
-      <div class="card-body border-bottom py-3">
-        <div class="d-flex">
-          <div class="text-secondary">
-            Show
-            <div class="mx-2 d-inline-block">
-              <input
-                type="number"
-                class="form-control form-control-sm"
-                v-model="perPage"
-                size="3"
-                aria-label="Product per page"
-              />
+  <div class="mx-1">
+    <div class="col-12">
+      <div class="card">
+        <div class="card-header">
+          <h3 class="card-title">Products</h3>
+        </div>
+        <div class="card-body border-bottom py-3">
+          <div class="d-flex">
+            <div class="text-secondary">
+              Show
+              <div class="mx-2 d-inline-block">
+                <input
+                  type="number"
+                  class="form-control form-control-sm"
+                  v-model="perPage"
+                  size="3"
+                  aria-label="Product per page"
+                />
+              </div>
+              entries
             </div>
-            entries
-          </div>
-          <div class="ms-auto text-secondary">
-            Search:
-            <div class="ms-2 d-inline-block">
-              <input
-                type="text"
-                class="form-control form-control-sm"
-                v-model="searchValue"
-                aria-label="Search invoice"
-              />
+            <div class="ms-auto text-secondary">
+              Search:
+              <div class="ms-2 d-inline-block">
+                <input
+                  type="text"
+                  class="form-control form-control-sm"
+                  v-model="searchValue"
+                  aria-label="Search invoice"
+                />
+              </div>
             </div>
           </div>
         </div>
-      </div>
-      <div class="table-responsive">
-        <table class="table card-table table-vcenter text-nowrap datatable">
-          <thead>
-            <tr>
-              <th>No</th>
-              <th>Product ID</th>
-              <th>Type</th>
-              <th>Brand</th>
-              <th>Model</th>
-              <th>Capacity</th>
-              <th>Quantity</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr v-for="(product, index) in products" :key="index">
-              <td>{{ product.id }}</td>
-              <td>
-                <span class="text-secondary">{{ product.product_id }}</span>
-              </td>
-              <td>{{ product.type }}</td>
-              <td>{{ product.product_model.brand.name }}</td>
-              <td>{{ product.product_model.name }}</td>
-              <td>{{ product.capacity }}</td>
-              <td>{{ product.quantity }}</td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
-      <div class="card-footer d-flex align-items-center">
-        <p class="m-0 text-secondary">
-          Showing <span>{{ pagination.from }}</span> to <span>{{ pagination.to }}</span> of
-          <span>{{ pagination.total }}</span> entries
-        </p>
-        <ul class="pagination m-0 ms-auto">
-          <Bootstrap5Pagination :data="pagination" @pagination-change-page="getProducts" />
-        </ul>
+        <div class="table-responsive">
+          <table class="table card-table table-vcenter text-nowrap datatable">
+            <thead>
+              <tr>
+                <th>No</th>
+                <th>Product ID</th>
+                <th>Type</th>
+                <th>Brand</th>
+                <th>Model</th>
+                <th>Capacity</th>
+                <th>Quantity</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr v-for="(product, index) in products" :key="index">
+                <td>{{ product.id }}</td>
+                <td>
+                  <span class="text-secondary">{{ product.product_id }}</span>
+                </td>
+                <td>{{ product.type }}</td>
+                <td>{{ product.product_model.brand.name }}</td>
+                <td>{{ product.product_model.name }}</td>
+                <td>{{ product.capacity }}</td>
+                <td>{{ product.quantity }}</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+        <div class="card-footer d-flex align-items-center">
+          <p class="m-0 text-secondary">
+            Showing <span>{{ pagination.from }}</span> to <span>{{ pagination.to }}</span> of
+            <span>{{ pagination.total }}</span> entries
+          </p>
+          <ul class="pagination m-0 ms-auto">
+            <Bootstrap5Pagination :data="pagination" @pagination-change-page="getProducts" />
+          </ul>
+        </div>
       </div>
     </div>
   </div>
